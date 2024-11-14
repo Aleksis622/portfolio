74c39dacf10f2549a5a94a65f0f38f46c7f51ff5
Skolnieks <skolnieks.pjvt@pjvt.local>
2024-11-14 10:39:28 +0200

    new

---
+++ ascii kods uzdevums.js
@@ -0,0 +1,6 @@
+function click(event){
+  // let f=document.getElementById("symbol").;
+  //console.log(event);
+  alert(event.keyCode);
+}
+document.addEventListener("keydown", click);
---
+++ relax_function (1).js
@@ -0,0 +1,48 @@
+window.addEventListener('click', function () {
+    
+    var audio = document.getElementById("meadow");
+    audio.play();
+})
+window.addEventListener('dblclick', function () {
+    var audio = document.getElementById("meadow");
+    audio.pause();
+    audio.currentTime = 0; // Optionally reset the audio to the beginning
+});
+window.addEventListener('click', function () {
+    
+    var audio = document.getElementById("jungle2");
+    audio.play();
+})
+window.addEventListener('dblclick', function () {
+    var audio = document.getElementById("jungle2");
+    audio.pause();
+    audio.currentTime = 0;})
+    window.addEventListener('click', function () {
+    
+        var audio = document.getElementById("beach2");
+        audio.play();
+    })
+    window.addEventListener('dblclick', function () {
+        var audio = document.getElementById("beach2");
+        audio.pause();
+        audio.currentTime = 0;})
+
+        window.addEventListener('click', function () {
+    
+            var audio = document.getElementById("forest2");
+            audio.play();
+        })
+        window.addEventListener('dblclick', function () {
+            var audio = document.getElementById("forest2");
+            audio.pause();
+            audio.currentTime = 0;})
+
+            window.addEventListener('click', function () {
+    
+                var audio = document.getElementById("winter2");
+                audio.play();
+            })
+            window.addEventListener('dblclick', function () {
+                var audio = document.getElementById("winter2");
+                audio.pause();
+                audio.currentTime = 0;})
